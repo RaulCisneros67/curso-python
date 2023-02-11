b6fe420f738c303f92d7d23501a89c89db6f7f9a
RaulCisneros67 <cisnerosraul139@gmail.com>
2023-02-10 21:10:07 -0600

    agregue otra practica

---
+++ Pruebas/banco.py
@@ -0,0 +1,25 @@
+monto = 5000
+
+co = monto 
+
+
+
+print("tu saldo es de:" , monto)
+
+print("Deseas hacer un retiro?")
+respuesta = input()
+
+
+
+
+if respuesta == 'si':
+        print("Cantidad a retirar")
+        dinero = int(input())
+        
+        print("Tu saldo restante es")
+        print(dinero-co)
+    
+else:
+    print("vallase muvgo ala vergaaaaaaa")
+    
+    
