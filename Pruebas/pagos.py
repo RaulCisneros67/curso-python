d43f7bf320b5c118bdfa9f5b41fe1a30c44e3cfa
RaulCisneros67 <cisnerosraul139@gmail.com>
2023-03-08 18:15:40 -0600

    cambio nuevo

--- Pruebas/pagos.py
+++ Pruebas/pagos.py
@@ -1,20 +1,20 @@
-# tarjeta = 123456
-# nip = 4819
-# monto = 5000
+tarjeta = 123456
+nip = 4819
+monto = 5000
 
 
 
 
-# datos = int(input("Numero de tarjeta: "))
+datos = int(input("Numero de tarjeta: "))
 
-# if datos == tarjeta:
-#     print("bienvenido señor Raul gilberto")
-#     con = int(input("Nip: "))
-#     if con == nip:
-#         print(f"Acceso correcto este es su monto: {monto}MXN ")
-#     else:
-#         print("El nip es incorrecto")
-# else:
-#     print("El numero de targeta no existe")
+if datos == tarjeta:
+    print("bienvenido señor Raul gilberto")
+    con = int(input("Nip: "))
+    if con == nip:
+        print(f"Acceso correcto este es su monto: {monto}MXN ")
+    else:
+        print("El nip es incorrecto")
+else:
+    print("El numero de targeta no existe")
 
 
